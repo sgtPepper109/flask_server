cb5b1b620b9eb90bb8057ad0b24e3fd96656f477
sgtPepper109 <a.r.shaikh1940@gmail.com>
2023-08-28 08:52:42 +0530

    server

--- server.py
+++ server.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 cors = CORS(app)
 
 # instead of using a database, all the details of the videos saved in Assets will be stored in this variable
+video_info_store = dict()
 video_store = dict()
+subtitles_store = dict()
 unique_id = 0
 current_video_upload_id = 0
 
@@ -18,7 +20,7 @@ assets_location = './Assets/'
 
 @app.route("/upload_file", methods=['POST'])
 def upload_file():
-    global video_store
+    global video_info_store
     global unique_id
     global current_video_upload_id
 
@@ -37,18 +39,21 @@ def upload_file():
     file_save_location = assets_location + file_name
 
     # if there exists a file with this name, then 
-    if (os.path.isfile(file_save_location)):
-        return make_response('File already exists')
+    # if (os.path.isfile(file_save_location)):
+    #     return make_response('File already exists')
 
     # else:
     # write (binary) video data received from frontend to the video file to be saved in Assets
-    with open(file_save_location, "wb") as out_file:
+    # with open(file_save_location, "wb") as out_file:
         # request.data holds the raw video file data
-        out_file.write(request.data)
+        # out_file.write(request.data)
     
     # unique id must be unique. Hence auto incremented value will be used to store the details
     unique_id += 1
-    video_store[unique_id] = {
+
+    video_store[unique_id] = request.data
+
+    video_info_store[unique_id] = {
         'file_name': file_name,
         'file_extension': file_extension,
         'location': file_save_location,
@@ -63,7 +68,7 @@ def upload_file():
 
 @app.route("/uploadVtt", methods=['POST'])
 def uploadVtt():
-    global video_store
+    global video_info_store
     global unique_id
     global current_video_upload_id
 
@@ -75,42 +80,43 @@ def uploadVtt():
 
     # file to be saved to this location (Assets folder)
     vtt_file_save_location = assets_location + vtt_file_name
-    video_store[unique_id]['subtitle_location'] = vtt_file_save_location
+    video_info_store[unique_id]['subtitle_location'] = vtt_file_save_location
+    subtitles_store[unique_id] = request.data
 
     # if there exists a file with this name, then 
-    if (os.path.isfile(vtt_file_save_location)):
-        return make_response('File already exists')
+    # if (os.path.isfile(vtt_file_save_location)):
+    #     return make_response('File already exists')
 
     # else:
     # write (binary) video data received from frontend to the video file to be saved in Assets
-    with open(vtt_file_save_location, "wb") as out_file:
+    # with open(vtt_file_save_location, "wb") as out_file:
         # request.data holds the raw video file data
-        out_file.write(request.data)
+        # out_file.write(request.data)
     
     
     # send acknowledgement response
     # return send_filo_file_save_location, download_name=file_name, mimetype="video/" + file_extension)
-    print(video_store)
+    print(video_info_store)
     return make_response('Subtitles saved successfully')
 
 
 # Get information (features) of all videos
 @app.route('/getVideoStats')
 def getVideoStats():
-    global video_store
+    global video_info_store
 
     # response object (array)
     video_info_array = list()
 
     # for all the videos in the video store
-    for i in video_store:
+    for i in video_info_store:
         # to add this to the array of responses
         video_info = {
-            'filename': video_store[i]['file_name'],
-            'file_extension': video_store[i]['file_extension'],
-            'title': video_store[i]['title'],
+            'filename': video_info_store[i]['file_name'],
+            'file_extension': video_info_store[i]['file_extension'],
+            'title': video_info_store[i]['title'],
             'id': i,
-            'subtitles_file': video_store[i]['subtitle_location'],
+            'subtitles_file': video_info_store[i]['subtitle_location'],
         }
         video_info_array.append(video_info)
     
@@ -122,45 +128,47 @@ def getVideoStats():
 # request parameter: id
 @app.route('/getVideo')
 def getVideo():
-    global video_store
+    global video_info_store
 
     # parse request parameters
     args = request.args.to_dict()
     # get id from request parameters
     id = int(args['id'])
 
-    # get video file location/path (saved in assets) (This is stored in 'video_store' variable)
-    file_location_to_send = video_store[id]['location']
-    # similarly get file_name
-    file_name = video_store[id]['file_name']
-    # similarly get file_extension
-    file_extension = video_store[id]['file_extension']
-    
-    # return video file with the help of 'send_file' method in flask
-    return send_file(file_location_to_send, download_name=file_name, mimetype="video/" + file_extension)
+    # send file stored in video_store variable as bytes only
+    response = make_response(video_store[id])
+    response.headers.set('Content-Type', 'video/mp4')
+    response.headers.set(
+        'Content-Disposition', 'attachment', filename=video_info_store[id]['file_name'])
+    return response
 
 
 # get subtitles file to show in client page
 # request parameter: id
 @app.route('/getSubtitles')
 def getSubtitles():
-    global video_store
+    global video_info_store
 
     # parse request parameter
     args = request.args.to_dict()
     # get id from request parameters
     id = int(args['id'])
 
-    # get subtitles file location/path (saved in assets) (This is stored in 'video_store' variable)
-    subtitles_file_location_to_send = video_store[id]['subtitle_location']
-    # return subtitles file with the help of 'send_file' method in flask
-    return send_file(subtitles_file_location_to_send)
+    # send send file stored in subtitles_store variable
+    response = make_response(subtitles_store[id])
+    response.headers.set(
+        'Content-Disposition', 'attachment')
+    return response
 
 
 # Just a test (Home) route
 @app.route('/')
-def test():
+def home():
     return 'TOUCHCORE ASSESSMENT FLASK SERVER. Name: Abdulrahim Shaikh'
+
+@app.route('/test')
+def test():
+    return 'Test route'
 
 
 if __name__ == "__main__":
